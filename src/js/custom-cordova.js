2ec93bc1db08828b3b0342d01e7a512fc2b93e46
DonCastillo <don.qcastillo@yahoo.com>
2022-12-01 23:24:36 -0700

    working pedometer

--- src/js/custom-cordova.js
+++ src/js/custom-cordova.js
@@ -1,6 +1,9 @@
 import $ from 'jquery';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+// import stepper from '../../cordova/plugins/cordova-plugin-stepper/www/stepper';
+// import {stepper} from '@felicienfrancois/cordova-plugin-stepper';
+// import stepper from ''
 mapboxgl.accessToken = 'pk.eyJ1IjoiZG9uY2FzdGlsbG8iLCJhIjoiY2p6cTN2eDhwMHdqZjNvanNhMGk0cTRhaCJ9.rfa3wF7Pz3VRhpyENHGCpQ'
 
 var customCordovaApp = {
@@ -23,7 +26,7 @@ var customCordovaApp = {
             // initialize map object
             map = new mapboxgl.Map({
                 container: 'map',
-                style: 'mapbox://styles/doncastillo/clb0kfrb7003g15mid0eff3kb',
+                style: 'mapbox://styles/mapbox/streets-v12',
                 center: [long, lat],
                 zoom: zoom
             });
@@ -47,10 +50,59 @@ var customCordovaApp = {
 
 
     },
+    ped: function () {
+        let prevMagnitude = 0;
+        let stepCount = 0;
+        let threshold = 6;
+        let flag = 0;
+
+        window.addEventListener('devicemotion', event => {
+            const {x, y, z} = event.acceleration;
+            let curMagnitude = Math.sqrt(x * x  + y * y + z * z);
+            let magnitudeDelta = curMagnitude + prevMagnitude / 2;
+            prevMagnitude = curMagnitude;
+
+            if(magnitudeDelta > threshold && flag == 0) {
+                stepCount++;
+                flag = 1;
+            }else if(magnitudeDelta > threshold && flag == 1) {
+                // don't count
+            }else{
+
+            }
+            
+            if(magnitudeDelta < threshold && flag == 1){
+                flag = 0;
+            }
+
+            if(stepCount < 0) {
+                stepCount = 0;
+            }
+
+            $('#map').html(stepCount)
+
+            console.log('step count: ', stepCount);
+
+
+        }, true)
+
+
+
+
+
+
+    },
     init: function (f7) {
+        console.log('init customcordova app')
         customCordovaApp.f7 = f7;
 
-        customCordovaApp.geolocation();
+        document.addEventListener('deviceready', () => {
+            // console.log('init geolocation')
+            // customCordovaApp.geolocation();
+            console.log('init pedo')
+            customCordovaApp.ped();
+        });
+
     }
 }
 
